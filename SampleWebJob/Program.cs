4afcde29d5a0b00c03b948ca34d5412a666a5a9c
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2017-03-18 15:53:22 -0300

    Added console app as webjob

---
+++ SampleWebJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! from a Sample ASP.NET Core WebJob");
+        }
+    }
+}
